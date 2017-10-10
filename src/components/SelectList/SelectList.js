1b24ca81bb0fc4d1d3e139c51feb6027233b6492
Connor Katz <connorkatz@gmail.com>
2017-10-09 23:34:21 -0700

    Added State to SelectList

--- src/components/SelectList/SelectList.js
+++ src/components/SelectList/SelectList.js
@@ -1,20 +1,37 @@
 import React, { Component } from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import './SelectList.css';
 
 class SelectList extends Component {
+
+   state = {
+      active: false,
+   }
+
+   toggleActive = () => {
+      this.setState(state => ({
+         active: !state.active
+      }))
+   }
+
    render() {
+      const { active } = this.state;
+      const { name } = this.props;
+
       return (
-         <div className="select-list">
-            <span>{this.props.name}</span>
+         <div className={`select-list ${active ? "active" : ""}`}>
+            <label onClick={this.toggleActive}>{name}</label>
+            <h5></h5>
             <ul>
                <li><Link to='/'>All</Link></li>
-               <li>Cat 1</li>
+               <li></li>
                <li>Cat 2</li>
                <li>Cat 3</li>
             </ul>
          </div>
       )
    }
+
 }
 
 export default SelectList;
