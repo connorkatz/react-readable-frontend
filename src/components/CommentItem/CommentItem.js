29c52cff101139dac1e6cb6f37fbf9344fa49bc4
Connor Katz <connor.katz@gmail.com>
2017-11-08 18:59:19 -0800

    updates

--- src/components/CommentItem/CommentItem.js
+++ src/components/CommentItem/CommentItem.js
@@ -1,20 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 import VoteScore from '../VoteScore/VoteScore';
 import './CommentItem.css';
 
-const CommentItem = (props) => {
-   const {body, author, voteScore} = props.comment;
-   return (
-      <div className="comment-item">
-         <div className="layout-block-1">
-            <p>{body}</p>
-            <address>{author}</address>
+class CommentItem extends Component {
+   state = {
+      editMode: false
+   }
+
+   toggleEditMode = () => {
+      this.setState(
+         {
+            editMode: true,
+         }
+      )
+   }
+
+   render() {
+      const { body, author, voteScore } = this.props.comment;
+      const { editMode } = this.state;
+      return (
+         <div className="comment-item">
+            {editMode ? (
+               <div className="comment-edit">
+                  
+               </div>
+            ) : (
+               <div className="comment-content">
+                  <div className="layout-block-1">
+                     <p>{body}</p>
+                     <address>{author}</address>
+                     <div className="comment-actions">
+                        <span onClick={this.toggleEditMode}>Edit</span>
+                        <i>&nbsp; | &nbsp;</i>
+                        <span>Delete</span>
+                     </div>
+                  </div>
+                  <div className="layout-block-2">
+                     <VoteScore votes={voteScore} />
+                  </div>
+               </div>
+            )}
          </div>
-         <div className="layout-block-2">
-            <VoteScore votes={voteScore}/>
-         </div>
-      </div>
-   )
+      )
+   }
 }
 
 export default CommentItem;
